057b7d349d39dc62389b4b91f2c70b944754c10a
Henry Lv <henrylv206@qq.com>
2018-04-17 16:41:07 +0800

    add bitree level traverse

---
+++ src/com/alicode/bitree/LevelorderTraversal.java
@@ -0,0 +1,57 @@
+package com.alicode.bitree;
+
+import java.util.*;
+
+// Definition for a binary tree node.
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
+
+public class LevelorderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> values = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+
+        if (root == null) {
+            return values;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        TreeNode last = root;
+        TreeNode nlast = null;
+
+        TreeNode temp = null;
+        while (!queue.isEmpty()) {
+            temp = queue.poll();
+
+            list.add(temp.val);
+
+            if (temp.left != null) {
+                queue.add(temp.left);
+
+                nlast = temp.left;
+            }
+            if (temp.right != null) {
+                queue.add(temp.right);
+
+                nlast = temp.right;
+            }
+
+            if (temp == last) {
+                values.add(list);
+                list = new ArrayList<Integer>();
+
+                last = nlast;
+                //nlast = null;
+            }
+        }
+
+        return values;
+    }
+
+}
